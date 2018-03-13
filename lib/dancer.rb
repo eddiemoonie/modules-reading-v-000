83277380020ef440bd02d5cf76dc568b12f58c31
Eddie Moon <eddiemoonie@gmail.com>
2018-03-13 20:12:29 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,7 +4,6 @@ require_relative './dance_module.rb'
 class Dancer
 
   include Dance
-  extend MetaDancing
 
   attr_accessor :name
 
@@ -12,4 +11,8 @@ class Dancer
     @name = name
   end
 
+end
+
+class Dancer
+  extend MetaDancing
 end
